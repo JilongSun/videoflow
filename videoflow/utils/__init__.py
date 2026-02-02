74141476b29a032b7f6fd951e27c071167f30c63
孙吉龙 <965640067@qq.com>
2026-02-02 15:16:24 +0800

    feat: Add douyin download IMP

--- videoflow/utils/__init__.py
+++ videoflow/utils/__init__.py
@@ -1,1 +1,7 @@
 from .logger_config import log
+import sys, os
+
+# 将 downloader 目录添加到 Python 路径中
+downloader_path = os.path.join(os.path.dirname(__file__), "downloader")
+print(downloader_path)
+sys.path.insert(0, downloader_path)
